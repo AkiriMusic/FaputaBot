014a680c15e653d290ec2d8179af878e940e3f8d
Akiwi <jamesofthesouthernrealm@gmail.com>
2023-05-04 12:51:34 -0400

    Initial Commit

---
+++ Logger.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+
+namespace FapuButt;
+
+public class Logger {
+    public static void Log(string message) {
+        LogAsync(new LogMessage(LogSeverity.Info, "Fapu", message));
+    }
+    
+    public static void Error(string message) {
+        LogAsync(new LogMessage(LogSeverity.Error, "Fapu", message));
+    }
+    
+    public static void Error(Exception exception) {
+        LogAsync(new LogMessage(LogSeverity.Error, "Fapu", exception.Message, exception));
+    }
+    
+    public static void Error(string message, Exception exception) {
+        LogAsync(new LogMessage(LogSeverity.Error, "Fapu", message, exception));
+    }
+    
+    public static void Warn(string message) {
+        LogAsync(new LogMessage(LogSeverity.Warning, "Fapu", message));
+    }
+    
+    public static void Debug(string message) {
+        LogAsync(new LogMessage(LogSeverity.Debug, "Fapu", message));
+    }
+
+    public static Task LogAsync(LogMessage message) {
+        var task = new Task(() => {
+            Console.WriteLine(message.ToString());
+        });
+        
+        task.Start();
+        
+        return task;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Net.Mime;
+using Discord;
+using Discord.WebSocket;
+using FapuButt;
+
+public static class Faputa
+{
+    private static DiscordSocketClient client;
+
+        public static async Task Main(string[] args)
+    {
+        await Run();
+    }
+
+    private static async Task Run()
+    {
+        string token = await File.ReadAllTextAsync("token.txt");
+        client = new DiscordSocketClient();
+        client.Log += Logger.LogAsync;
+        client.Ready += Ready;
+        client.SlashCommandExecuted += onCommand;
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        await Task.Delay(-1);
+        
+    }
+
+    private static async Task Ready()
+    {
+        Logger.Log("Ready");
+        var sosuCommand = new SlashCommandBuilder()
+        {
+            Name = "sosu",
+            Description = "Fapu"
+        };
+        sosuCommand.AddOption(new SlashCommandOptionBuilder()
+        {
+            Name = "count",
+            Description = "Amount of Fapu",
+            Type = ApplicationCommandOptionType.Integer
+        });
+            await client.CreateGlobalApplicationCommandAsync(sosuCommand.Build());
+    }
+
+    private static async Task onCommand(SocketSlashCommand interaction)
+    {
+        switch (interaction.Data.Name.ToLower())
+        {
+            case "sosu":
+                await interaction.RespondAsync("Sosu Sending...", ephemeral:true);
+                long count = 1;
+                interaction.Data.Options.Where(option => option.Name == "count").ToList().ForEach(option => {
+                    count = (long)option.Value;
+                });
+                count = Math.Clamp(count, 1, 10);
+                var sent = new List<long>();
+                for (long i = 0; i < count; i++)
+                {
+                    int rng = new Random().Next(Images.Sosu.Length);
+                    while (sent.Contains(rng))
+                    {
+                        rng = new Random().Next(Images.Sosu.Length);
+                    }
+                    await interaction.Channel.SendMessageAsync(Images.Sosu[rng]);
+                    sent.Add(rng);
+                }
+                break;
+        }
+    }
+}
